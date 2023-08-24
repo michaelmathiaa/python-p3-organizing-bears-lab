e67145eb5247f08e9c82e69c772185d51236f41d
Michael Mathias <michaelscodingjourney@gmail.com>
2023-08-23 20:58:32 -0700

    Changed boolean values to numbers

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "Brown", "Friendly", True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 8, "M", "Black", "Aggressive", False);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, "F", "Black", "Playful", True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 7, "M", "Brown", "Playful", True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 9, "F", "Brown", "Friendly", True);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 18, "M", "White", "Friendly", False);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 14, "F", "Brown", "Aggressive", False);
-INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "M", "White", "Aggressive", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "Brown", "Friendly", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 8, "M", "Black", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, "F", "Black", "Playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 7, "M", "Brown", "Playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 9, "F", "Brown", "Friendly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 18, "M", "White", "Friendly", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 14, "F", "Brown", "Aggressive", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "M", "White", "Aggressive", 1);
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -19,7 +19,7 @@ select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = ""
         name,
         age
     FROM bears
-    WHERE alive=True
+    WHERE alive=1
     ORDER BY
         age;
 """
