d7c040ab4dd037e1c1ddc69c35bb4c13b4a60e78
Michael Mathias <michaelscodingjourney@gmail.com>
2023-08-23 20:48:38 -0700

    Changed = to IS

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -19,7 +19,7 @@ select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = ""
         name,
         age
     FROM bears
-    WHERE alive=TRUE
+    WHERE alive IS TRUE
     ORDER BY
         age;
 """
