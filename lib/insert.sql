d9fbad57604a2abb0ed4d8a112c0aeebc0ad6753
Michael Mathias <michaelscodingjourney@gmail.com>
2023-08-23 20:33:42 -0700

    Passed twelve tests for lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "Brown", "Friendly", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 8, "M", "Black", "Aggressive", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, "F", "Black", "Playful", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 7, "M", "Brown", "Playful", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 9, "F", "Brown", "Friendly", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 18, "M", "White", "Friendly", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 14, "F", "Brown", "Aggressive", False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "M", "White", "Aggressive", True);
